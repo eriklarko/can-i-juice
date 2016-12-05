548370ec3d51d34745a0575d2929ee57eb169047
Erik Larkö <erik@larko.se>
2016-12-05 23:26:30 +0100

    UI end-point

--- web-server/index.js
+++ web-server/index.js
@@ -7,13 +7,17 @@ function logPackageCheck(response) {
     delete response.package;
 }
 
-app.get('/:packageName', function (req, res) {
+app.get('/check/:packageName', function (req, res) {
     const packageName = req.params.packageName;
     const response = check(packageName);
     logPackageCheck(response);
     res.send(response);
-})
+});
+
+app.get('/', function (req, res) {
+    res.send("Here be pretty react file. Oh yeah");
+});
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+    console.log('App listening on port 3000!')
+});
