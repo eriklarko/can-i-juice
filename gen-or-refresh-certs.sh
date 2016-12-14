5e8fe3b94b29d4b26d6031206c7f2125f33e597a
Erik Larkö <erik@larko.se>
2016-12-14 23:23:27 +0100

    Preparing for letsencrypt. Gotta buy that domain.. :)

--- gen-or-refresh-certs.sh
+++ gen-or-refresh-certs.sh
@@ -1,1 +1,3 @@
-echo 'Not implemented yet'
+set -eux
+HOST="can-i-juice.com"
+certbot certonly --standalone --renew-by-default --config-dir ./letsencrypt/conf --work-dir ./letsencrypt/work --logs-dir ./letsencrypt/logs --email erik@larko.se --agree-tos -d $HOST -d www.$HOST
