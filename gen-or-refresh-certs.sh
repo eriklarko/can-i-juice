6dd5f063382d2c4ab49a1849cc0ceb403a3cdc10
Erik Larkö <erik@larko.se>
2016-12-16 23:02:49 +0100

    Put the correct cert files in the correct place

--- gen-or-refresh-certs.sh
+++ gen-or-refresh-certs.sh
@@ -1,3 +1,6 @@
 set -eux
 HOST="can-i-juice.erik.la"
 certbot certonly --standalone --renew-by-default --config-dir ./letsencrypt/conf --work-dir ./letsencrypt/work --logs-dir ./letsencrypt/logs --email erik@larko.se --agree-tos -d $HOST -d www.$HOST
+
+sudo cp "$(pwd)/letsencrypt/conf/live/$HOST/fullchain.pem" certs/default.crt
+sudo cp "$(pwd)/letsencrypt/conf/live/$HOST/privkey.pem" certs/default.key
