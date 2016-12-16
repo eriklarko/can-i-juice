4f12e4bd1fdba5e60c8a86d2f7e62ef33b024e24
Erik Larkö <erik@larko.se>
2016-12-16 22:02:40 +0100

    changed domain

--- gen-or-refresh-certs.sh
+++ gen-or-refresh-certs.sh
@@ -1,3 +1,3 @@
 set -eux
-HOST="can-i-juice.com"
+HOST="can-i-juice.erik.la"
 certbot certonly --standalone --renew-by-default --config-dir ./letsencrypt/conf --work-dir ./letsencrypt/work --logs-dir ./letsencrypt/logs --email erik@larko.se --agree-tos -d $HOST -d www.$HOST
